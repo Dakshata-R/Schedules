ae4af1cd5bba0c537e7523a812ecb3200a3bcd41
Dakshata-R <dakshata277@gmail.com>
2025-03-11 10:47:07 +0530

    backend

--- backend/config/roles_db.js
+++ backend/config/roles_db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sandhiya',
+  password: 'dakshata',
   database: 'roles_management',
 });
 
